6c3b6031504dd866b00839d50e5a791902f6fcee
Nicholas DiMaria <ndimaria@stevens.edu>
2020-10-19 16:36:24 -0400

    Update MYSQL database with thermostat temperature and status

--- SmartNest/index.php
+++ SmartNest/index.php
@@ -1,70 +1,101 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <body>
-<?php
-session_start();
-$input=$_GET['input'];
-$page = $_SERVER['PHP_SELF'];
-$sec = "10";
+	
+	<?php
+		session_start();
+		$input=$_GET['input'];
+		$page = $_SERVER['PHP_SELF'];
+		$sec = "10";
 
-$hostname = "localhost";
-$username = "review_site";
-$password = "JxSLRkdutW";
-$db = "reviews";
+		$hostname = "localhost";
+		$username = "review_site";
+		$password = "JxSLRkdutW";
+		$db = "reviews";
 
-$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+		$dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
-if ($dbconnect->connect_error) {
-  die("Database connection failed: " . $dbconnect->connect_error);
-}
+		if ($dbconnect->connect_error) {
+			die("Database connection failed: " . $dbconnect->connect_error);
+		}
+	?>
+	
+	<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+	<table border="1" align="center">
+	<tr>
+	  <td>Date and Time</td>
+	  <td>Temp</td>
+	</tr>
 
-?>
-<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
-<table border="1" align="center">
-<tr>
-  <td>Date and Time</td>
-  <td>Temp</td>
-</tr>
+	<?php
 
-<?php
+	$query = mysqli_query($dbconnect, "SELECT * FROM tempLog ORDER BY datetime DESC LIMIT 1")
+	   or die (mysqli_error($dbconnect));
 
-$query = mysqli_query($dbconnect, "SELECT * FROM tempLog ORDER BY datetime DESC LIMIT 1")
-   or die (mysqli_error($dbconnect));
+	while ($row = mysqli_fetch_array($query)) {
+	  $temperature = $row['temperature'];
+	  echo
+	   "<tr>
+		<td>{$row['datetime']}</td>
+		<td>{$row['temperature']}</td>
+	   </tr>\n";
+	}
+	
+	$query = mysqli_query($dbconnect, "SELECT * FROM info ORDER BY ID DESC LIMIT 1")
+	   or die (mysqli_error($dbconnect));
+	   
+	$row = mysqli_fetch_array($query);
+	$_SESSION["input"] = $row['temp'];
+	$_SESSION["status"] = $row['status'];
+	
+	echo "<div align='center'>The temperature is currently set to: </div>";
+	echo "<h1 align='center'>".$_SESSION["input"]."</h1>";
+	echo "<div align='center'>The AC is currently ".$_SESSION["status"]."<br><br></div>";
+	
+	if($_SESSION["input"] < $temperature && ($_SESSION["status"] == "TRANSITIONING" || $_SESSION["status"] == "OFF")){
+		$command = escapeshellcmd('/home/pi/NickIoT/SmartNest/turnOn.py');
+		$output=shell_exec($command);
+		$_SESSION["status"] = "ON";
+		$sql="INSERT INTO info (temp, status) VALUES ('".$_SESSION["input"]."','".$_SESSION["status"]."')";
+		if (mysqli_query($dbconnect, $sql)) {
+			echo "<div align='center'>The AC is turning ON. Status: ".$output."<br><br></div>";
+		} else {
+			echo "<div align='center'>Error: ". $sql ."". mysqli_error($conn)."<br><br></div>";
+		}
+		
+	}
+	else if($_SESSION["input"] > $temperature && ($_SESSION["status"] == "TRANSITIONING" || $_SESSION["status"] == "ON")){
+		$command = escapeshellcmd('/home/pi/NickIoT/SmartNest/turnOff.py');
+		$output=shell_exec($command);
+		$_SESSION["status"] = "OFF";
+		$sql="INSERT INTO info (temp, status) VALUES ('".$_SESSION["input"]."','".$_SESSION["status"]."')";
+		if (mysqli_query($dbconnect, $sql)) {
+			echo "<div align='center'>The AC is turning OFF. Status: ".$output."<br><br></div>";
+		} else {
+			echo "<div align='center'>Error: " . $sql . "" . mysqli_error($conn)."<br><br></div>";
+		}
+	}
+	
+	if (isset($_POST['SetTemp'])) {
+	  $_SESSION["input"] = intval($_POST['input']);
+	  $_SESSION["status"] = "TRANSITIONING";
+	  $sql="INSERT INTO info (temp, status) VALUES ('".$_SESSION["input"]."','".$_SESSION["status"]."')";
+	  if (mysqli_query($dbconnect, $sql)) {
+        echo  "<div align='center'>Changing thermostat temperature to: ".$_SESSION["input"]."<br><br></div>";
+      } else {
+        echo "<div align='center'>Error: " . $sql . "" . mysqli_error($conn)."<br><br></div>";
+      }
+      $dbconnect->close();
+	}
 
-while ($row = mysqli_fetch_array($query)) {
-  $temperature = $row['temperature'];
-  echo
-   "<tr>
-    <td>{$row['datetime']}</td>
-    <td>{$row['temperature']}</td>
-   </tr>\n";
-}
-echo $_SESSION["input"];
-if($_SESSION["input"] < $temperature){
-    $command = escapeshellcmd('/home/pi/IoT/SmartNest/turnOn.py');
-    $output=shell_exec($command);
-    echo "Turning on";
-    echo $output;
-}
-else{
-  if($_SESSION["input"] > $temperature)
-  {
-    $command = escapeshellcmd('/home/pi/IoT/SmartNest/turnOff.py');
-    $output=shell_exec($command);
-    echo "Turning off";
-    echo $output;
-  }
-}
-if (isset($_POST['SetTemp'])) {
-  $_SESSION["input"] = $_POST['input'];
-  echo $_SESSION["input"];
-}
-
-?>
-</table>
- <form method="post">
-    <input type="text" name="input" id="input">
-    <button class="btn" name="SetTemp">Set Temp</button>&nbsp;
- </form>
+	?>
+	
+	</table>
+	<h1></h1>
+	<form method="post" align = "center">
+		<input type="text" name="input" id="input">
+		<button class="btn" name="SetTemp">Set Temp</button>&nbsp;
+	</form>
+	
 </body>
 </html>
